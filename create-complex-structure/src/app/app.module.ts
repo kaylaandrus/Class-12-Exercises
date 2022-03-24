62c48b75f36431f0908d021dc31d9c769ec638be
kaylaandrus <kaylam.andrus@gmail.com>
2022-03-24 18:37:07 -0500

    Class 12 Exercise 2

--- create-complex-structure/src/app/app.module.ts
+++ create-complex-structure/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { PostsComponent } from './home/posts/posts.component';
+import { FriendsListComponent } from './friends/friends-list/friends-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FriendsComponent,
+    MyProfileComponent,
+    PostsComponent,
+    FriendsListComponent
   ],
   imports: [
     BrowserModule
